574a9c0675ab144cfd736d9157327ec1f37a08ec
Tony Huang <t.tonyhuang@gmail.com>
2021-09-18 01:26:40 -0400

    add login UI

--- FE/screens/Login.js
+++ FE/screens/Login.js
@@ -1,13 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useContext, useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Platform,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import LoginInput from "../components/LoginInput";
+import FormButton from "../components/LoginButton";
+import SocialButton from "../components/OAuthButton";
 
-function LoginPage({navigation}){
-    return(
+const LoginPage = ({ navigation }) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Image
+        source={require("../assets/rn-social-logo.png")}
+        style={styles.logo}
+      />
+      <Text style={styles.text}>RN Social App</Text>
+
+      <LoginInput
+        labelValue={email}
+        onChangeText={(userEmail) => setEmail(userEmail)}
+        placeholderText="Email"
+        iconType="user"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+
+      <LoginInput
+        labelValue={password}
+        onChangeText={(userPassword) => setPassword(userPassword)}
+        placeholderText="Password"
+        iconType="lock"
+        secureTextEntry={true}
+      />
+
+      <FormButton buttonTitle="Sign In" />
+
+      <TouchableOpacity style={styles.forgotButton} onPress={() => {}}>
+        <Text style={styles.navButtonText}>Forgot Password?</Text>
+      </TouchableOpacity>
+
+      {Platform.OS === "android" ? (
         <View>
-            <Text>Login Page</Text>
+          <SocialButton
+            buttonTitle="Sign In with Facebook"
+            btnType="facebook"
+            color="#4867aa"
+            backgroundColor="#e6eaf4"
+          />
+
+          <SocialButton
+            buttonTitle="Sign In with Google"
+            btnType="google"
+            color="#de4d41"
+            backgroundColor="#f5e7ea"
+          />
         </View>
-    );
-}
+      ) : null}
+
+      <TouchableOpacity
+        style={styles.forgotButton}
+        onPress={() => navigation.navigate("Signup")}
+      >
+        <Text style={styles.navButtonText}>
+          Don't have an acount? Create here
+        </Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+};
 
 export default LoginPage;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    paddingTop: 50,
+  },
+  logo: {
+    height: 150,
+    width: 150,
+    resizeMode: "cover",
+  },
+  text: {
+    fontSize: 28,
+    marginBottom: 10,
+    color: "#051d5f",
+  },
+  navButton: {
+    marginTop: 15,
+  },
+  forgotButton: {
+    marginVertical: 35,
+  },
+  navButtonText: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#2e64e5",
+  },
+});
