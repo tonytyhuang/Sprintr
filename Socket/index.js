03ea98b2cee58f1c1f5ee8db3f760c29b6cc4902
Forrest Fan <forrest.c.fan@gmail.com>
2021-09-18 00:04:06 -0400

    add basic websocket code and html file to test client

--- Socket/index.js
+++ Socket/index.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const SocketServer = require('ws').Server;
+
 const app = express();
 const port = 3000;
+const server = app.listen(port, () => {
+    console.log("Listening at port " + port);
+});
+
+// Initialize websocket
+const wss = new SocketServer({ server })
 
 app.get('/', (req, res) => {
     res.send("Hello world");
 });
 
-app.listen(port, () => {
-    console.log("Listening at port " + port);
-});
+// When client connects
+wss.on('connection', (ws) => {
+    console.log("[Server] A client connected to the socket.");
+
+    ws.on('close', () => {
+        console.log("[Server] A client disconnected");
+    });
+
+    ws.on('error', (err) => {
+        console.log("[Server] An error occured: ");
+        console.log(err);
+    });
+
+    ws.on('message', (msg) => {
+        console.log("[Server] A message arrived: ");
+        console.log(JSON.parse(msg).text);
+    });
+})
