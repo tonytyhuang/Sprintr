d87632b924500fe6d961c51dee0c2aaec86b1d8f
Mer Zhang <mer.zhang01@mail.com>
2021-09-19 00:59:02 -0400

    page

---
+++ FE/screens/UserHistory.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import React, { useState, useContext, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Alert,
+  Dimensions,
+  ImageBackground,
+} from "react-native";
+
+export default function UserHistory() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
