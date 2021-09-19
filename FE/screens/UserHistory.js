6c48c2a8cec537e9c0d414287649a86eba681386
Tony Huang <t.tonyhuang@gmail.com>
2021-09-19 01:29:35 -0400

    add progress

--- FE/screens/UserHistory.js
+++ FE/screens/UserHistory.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
 import React, { useState, useContext, useEffect } from "react";
 import {
   StyleSheet,
@@ -12,9 +12,10 @@ import {
 } from "react-native";
 
 export default function UserHistory() {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
+  return (
+    <ImageBackground
+      style={styles.container}
+      source={require("../assets/achievement.png")}
+    ></ImageBackground>
+  );
 }
